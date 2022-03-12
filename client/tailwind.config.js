1fbdf4b3ee59a795bd34d62de87739a33b0136cd
Dev Raj <rehansingh.4522@gmail.com>
2022-03-12 23:40:46 +0530

    added login page

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        black: { 400: "#030808" },
+        green: { 400: "#47A650" },
+        yellow: { 400: "#CDB624" },
+        teal: { 400: "#205F58" },
+        white: { 400: "#F4F7E2" },
+        red: { 400: "#C33E30" },
+        grey: { 400: "#A0A7AF" },
+      },
+    },
+  },
+  plugins: [],
+};
